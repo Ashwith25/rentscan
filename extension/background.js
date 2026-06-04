fe516002907f1a1090886aa2138a8059ccda6d94
Ash <pashwith25@gmail.com>
2026-06-04 15:03:53 -0700

    fix: update Gemini API model to gemini-3.5-flash following deprecation of older models

--- extension/background.js
+++ extension/background.js
@@ -199,7 +199,7 @@ ${chunk.map((msg, idx) => `[${idx}]: "${msg.message.replace(/"/g, '\\"')}"`).joi
     }
   };
 
-  const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`, {
+  const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash:generateContent?key=${apiKey}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
